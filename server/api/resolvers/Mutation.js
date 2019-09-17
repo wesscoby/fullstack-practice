b0ac15597f6f3fb2282ea512fdd2db269af17cb0
WessCoby <cobygiven@gmail.com>
2019-09-17 10:51:09 +0000

    Added dependencies body-parser cookie-parser jsonwebtoken and passport-jwt

--- server/api/resolvers/Mutation.js
+++
@@ -1,9 +0,0 @@
-import { User } from '../../db/model'
-
-export const logout = async (parent, args, { logout }) => logout();
-
-export const login = async (parent, { email, password }, context) => {
-    const { user } = await context.authenticate('graphql-local', { email, password });
-    context.login(user);
-    return { user }
-}
