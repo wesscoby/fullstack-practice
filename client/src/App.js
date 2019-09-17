0396dd365d307a347a424f2ba71753de6701bc29
WessCoby <cobygiven@gmail.com>
2019-09-17 16:20:42 +0000

    Deleted some unwanted files from create-react-app defaults

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,11 @@
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <h1>Any place in your app!</h1>
-      <Formik
-        initialValues={{ email: 'cobygiven@gmail', password: '' }}
-        validate={values => {
-          let errors = {};
-          if (!values.email) {
-            errors.email = 'Required';
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = 'Invalid email address';
-          }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <Field type="email" name="email" />
-            <ErrorMessage name="email" component="div" />
-            <Field type="password" name="password" />
-            <ErrorMessage name="password" component="div" />
-            <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button>
-          </Form>
-        )}
-      </Formik>
-  </div>
+    <BrowserRouter>
+      
+  </BrowserRouter>
   );
 }
 
