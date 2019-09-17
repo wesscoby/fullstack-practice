bc1764fa7587c73c33a93b3d86a5ddcd0493ff28
WessCoby <cobygiven@gmail.com>
2019-09-17 10:56:56 +0000

    Added Mongoose Models

--- server/db/model/index.js
+++ server/db/model/index.js
@@ -1,6 +1,10 @@
 import { model as Model } from 'mongoose';
-import UserSchema from '../schema/User.schema'
+import UserSchema from '../schema/user.schema';
+import EventSchema from '../schema/event.schema';
+import BookingSchema from '../schema/booking.schema';
 
 module.exports = {
-    User: Model('User', UserSchema)
+    Event: Model('Event', EventSchema),
+    User: Model('User', UserSchema),
+    Booking: Model('Booking', BookingSchema)
 }
