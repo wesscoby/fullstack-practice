1007bf1ea222c9726fdea4d556bebe35521aa554
WessCoby <cobygiven@gmail.com>
2019-09-17 10:55:57 +0000

    Added Config file

--- server/config/index.js
+++ server/config/index.js
@@ -3,5 +3,7 @@ require('dotenv').config();
 module.exports = {
     PORT: process.env.PORT,
     SESSION_SECRET: process.env.SESSION_SECRET,
-    LocalDB_URI: process.env.LocalDB_URI
+    LocalDB_URI: `${process.env.LocalDB_URI}/${process.env.DB_NAME}`,
+    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET
 }
