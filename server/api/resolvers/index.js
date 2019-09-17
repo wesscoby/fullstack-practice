1b190205062b471c10543c02592e7b500936ac21
WessCoby <cobygiven@gmail.com>
2019-09-17 16:02:36 +0000

    Added Interface __typeResolver

--- server/api/resolvers/index.js
+++ server/api/resolvers/index.js
@@ -1,11 +1,13 @@
 import * as Query from './query.resolver';
 import * as Mutation from './mutation.resolver';
 import * as Scalar from './scalar.resolver';
+import * as Interface from './interface.resolver';
 
 const resolvers = {
         Query,
         Mutation,
-        ...Scalar
+        ...Scalar,
+        ...Interface
 }
 
 export default resolvers;
