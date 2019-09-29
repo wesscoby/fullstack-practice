1e993d735a27b54a53f700832695860c27fadffa
WessCoby <cobygiven@gmail.com>
2019-09-29 15:11:17 +0000

    Added static methods to event entity

--- server/entities/event.ts
+++ server/entities/event.ts
@@ -1,7 +1,9 @@
-import { prop, Typegoose, Ref } from '@hasezoey/typegoose';
+import { prop, Typegoose, Ref, ModelType, staticMethod } from '@hasezoey/typegoose';
 import { Field, ID, Float, ObjectType } from 'type-graphql';
 import User from './user';
 
+type EventModelType = ModelType<Event> & typeof Event;
+// type EventInstanceType = InstanceType<Event | any>;
 
 //! Event Class
 @ObjectType()
@@ -34,6 +36,36 @@ export class Event extends Typegoose {
 
     @Field()
     updatedAt: Date;
+
+    // Static Methods
+    @staticMethod
+    public static async getOne(
+        this: EventModelType,
+        id: string
+    ): Promise<Event | null> {
+        return await this
+                        .findById(id)
+                        .populate({
+                            path: 'creator',
+                            populate: [
+                                { path: 'createdEvents'}
+                            ]
+                        });
+    }
+
+    @staticMethod
+    public static async getAll(
+        this: EventModelType
+    ): Promise<Event[]> {
+        return await this
+                        .find({})
+                        .populate({
+                            path: 'creator',
+                            populate: [
+                                { path: 'createdEvents'}
+                            ]
+                        });
+    }
 }
 
 //! Event Model
