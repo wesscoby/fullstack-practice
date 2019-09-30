73b5bef90010b95cbede0373f765daec81f27d0c
WessCoby <cobygiven@gmail.com>
2019-09-30 11:25:05 +0000

    Style tweaks to Events page

--- client/src/pages/Events.tsx
+++ client/src/pages/Events.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { gql } from 'apollo-boost';
 import { graphql } from 'react-apollo';
 import Elipsis from '../components/loaders/Elipsis';
+import { MDBRow, MDBCol } from 'mdbreact';
+
+interface Creator {
+    name: string;
+    email: string;
+    id: string;
+}
 
 interface Event {
     id: string;
@@ -9,25 +16,46 @@ interface Event {
     description: string;
     date: string;
     price: number;
+    creator: Creator;
     createdAt: Date;
     updatedAt: Date;
+}
+
+const pageStyle = {
+    border: "1px solid #003398", 
+    borderRadius: "15px", 
+    marginBottom: "10px",
+    boxShadow: "2px 2px 5px 4px #eee"
 }
 
 const EventsPage = ({ data: { loading, events } }: any) => (
     <div>
         <h1>Events Page</h1>
-        {(loading) ? (<Elipsis color="#003398" />) : events.map(
-            (event: Event) => (<div 
-            style={{ border: "1px solid #003398", borderRadius: "15px", marginBottom: "5px" }} 
-            key={event.id}>
-                <div style={{ padding: "5px" }}>
-                    <h3>{event.title}</h3>
-                    <strong>{event.description}</strong>
-                    <p>{event.date}</p>
-                    <strong>¢{event.price}</strong>
-                </div>
-            </div>)
-        )}
+        <div>
+        {
+            (loading) ? 
+            (<Elipsis color="#003398" />) :
+            (<MDBRow className="d-flex flex-row justify-content-around">
+                {events.map(
+                    (event: Event) => (
+                        <MDBCol xs="10" sm="10" md="10" lg="5" xl="5"
+                            className="bg-dark text-primary" 
+                            style={pageStyle} 
+                            key={event.id}
+                        >
+                            <div style={{ padding: "10px" }}>
+                                <h3>{event.title}</h3>
+                                <p className="lead">{event.description}</p>
+                                <p>{event.date}</p>
+                                <strong>¢{event.price}</strong>
+                                <p>By {event.creator.name} <span>({event.creator.email})</span></p>
+                            </div>
+                        </MDBCol>
+                    )
+                )}
+            </MDBRow>)
+        }
+        </div>
     </div>
 )
 
